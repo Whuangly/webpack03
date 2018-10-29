7af036b0db3e0dc1815e0ba3244eea98e84b93ae
Whuangly <779020806@qq.com>
2018-10-29 23:24:46 +0800

    webpack demo02

---
+++ demo02/app/app.js
@@ -0,0 +1,8 @@
+console.log('webpack入口文件');
+
+
+let value = 'webpack';
+let getWebpack = v => v;
+let val = getWebpack(value);
+
+console.log('val ==>' , val);
---
+++ demo02/config/webpack.config.js
@@ -0,0 +1,13 @@
+
+
+module.exports = {
+	//入口文件配置
+	//entry 可以是一个字符串，也可以是一个对象
+   // entry: './app/app.js'
+
+   entry: {
+   	app: './app/app.js',
+   	a: './app/a.js'
+   }
+    
+}
